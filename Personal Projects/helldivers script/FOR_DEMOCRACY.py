d7a42579e8a00825e8bda406127500cba0c011ec
Hope4Worthy <b.deines@pm.me>
2026-01-31 17:17:05 -0700

    update helldivers script

--- Personal Projects/helldivers script/FOR_DEMOCRACY.py
+++ Personal Projects/helldivers script/FOR_DEMOCRACY.py
@@ -4,31 +4,16 @@ import os
 from stratagem_list import *
 
 # ********** loadouts ********** #
-loadout_attack_bots         = [eagle_clusterBomb, eagle_airstrike, support_railgun, backpack_personalShield]
-loadout_attack_bots_low     = [eagle_airstrike, orbital_railcannonStrike, orbital_precisionStrike, support_railgun]
-loadout_attack_bots_high    = [eagle_airstrike, orbital_railcannonStrike, support_railgun, backpack_personalShield]
-loadout_attack_bugs         = [eagle_airstrike, orbital_railcannonStrike, orbital_precisionStrike, backpack_personalShield]
-loadout_attack_bugs_high    = [eagle_airstrike, orbital_railcannonStrike, support_expendableAntiTank, backpack_personalShield]
 
-loadout_defense         = [sentry_mortar, sentry_autocannon, support_quasarCannon, backpack_personalShield]
+loadouts = [
+    ["Bots : Low",eagle_airstrike, orbital_railcannonStrike, orbital_precisionStrike, support_railgun],
+    ["Bots : High",eagle_airstrike, orbital_railcannonStrike, support_railgun, backpack_personalShield],
+    ["Bugs : Low",eagle_airstrike, orbital_railcannonStrike, orbital_precisionStrike, backpack_personalShield],
+    ["Bugs : High",eagle_airstrike, orbital_railcannonStrike, support_expendableAntiTank, backpack_personalShield],
+    ["Defense",sentry_autocannon, orbital_railcannonStrike, support_quasarCannon, backpack_personalShield]
+]
 
-active_loadout = loadout_attack_bugs_high
-
-# set to none if no active global stratagem
-stratagemG      = none
-
-# overide loadout stratagem
-overide1 = none
-overide2 = none 
-overide3 = none 
-overide4 = none
-
-# ********** Set strategem ********** #
-
-stratagem1 = active_loadout[0] if overide1 == none else overide1
-stratagem2 = active_loadout[1] if overide2 == none else overide2
-stratagem3 = active_loadout[2] if overide3 == none else overide3
-stratagem4 = active_loadout[3] if overide4 == none else overide4
+active_loadout = 0
 
 # ********** Strategem keys ********** #
 stratagem1_key      = pynput.keyboard.Key.home
@@ -39,13 +24,12 @@ stratagemG_key      = '5'
 
 resupply_key    = pynput.keyboard.Key.insert
 reinforce_key   = pynput.keyboard.Key.delete
-hellbomb_key    = pynput.keyboard.Key.f4
 
 stratagem_toggle = pynput.keyboard.Key.alt_l
 
 # ********** Varriables ********** #
 
-key_delay   = 0.06 #  adjusts speed at which strategems inputs are used
+key_delay   = 0.02 #  adjusts speed at which strategems inputs are used
 flag        = True # set for starting state
 input       = pynput.keyboard.Controller()
 
@@ -56,9 +40,9 @@ def toggle_print(): # print message in toggle
     
     os.system('clear')
     if(flag):
-        print("***** ACTIVE *****")
+        print("ACTIVE :", loadouts[active_loadout][0])
     else:
-        print("***** INACTIVE *****")
+        print("INACTIVE")
 
 def execute(strategem): # execute strategem
     global key_delay
@@ -77,8 +61,11 @@ def on_active_quit(): # close threads and quit
     listner.stop()
     hotkey_quit.stop()
     hotkey_toggle.stop()
+    hotkey_next.stop()
+    hotkey_prev.stop()
+
     os.system('clear')
-    exit()
+    os._exit(0)
 
 def on_active_toggle(): # toggle active / inactive
     global flag
@@ -86,30 +73,43 @@ def on_active_toggle(): # toggle active / inactive
     flag = not flag
     toggle_print()
 
+def next_loadout():
+    global active_loadout
+    active_loadout = (active_loadout + 1)
+    
+    if(active_loadout >= len(loadouts)): active_loadout = 0
+
+    toggle_print()
+
+def prev_loadout():
+    global active_loadout
+    active_loadout = (active_loadout - 1)
+    
+    
+    if(active_loadout < 0): active_loadout = len(loadouts) - 1
+
+    toggle_print()
+
 def on_press(key): # listner function
     if(flag):
         # ***** strategem 1 ***** #
         if(key == stratagem1_key):
-            execute(stratagem1)
+            execute(loadouts[active_loadout][1])
         # ***** stragem 2 ***** #
         if(key == stratagem2_key):
-            execute(stratagem2)
+            execute(loadouts[active_loadout][2])
         # ***** stragem 3 ***** #
         if(key == stratagem3_key):
-            execute(stratagem3)
+            execute(loadouts[active_loadout][3])
         # ***** stragem 4 ***** #
         if(key == stratagem4_key):
-            execute(stratagem4)
-        # ***** global stratagem ***** #
-        if(key == stratagemG_key):
-            execute(stratagemG)
+                execute(loadouts[active_loadout][4])
 
         # ***** mission stratagems ***** #
         if(key == resupply_key):
             execute(mission_resupply)
         if(key == reinforce_key): 
             execute(mission_reinforce)
-            execute(mission_hellbomb)
 
 # ********** Startup Script ********** #
 
@@ -117,15 +117,21 @@ toggle_print()
 
 # define program listners
 hotkey_quit     = pynput.keyboard.GlobalHotKeys({'<ctrl>+<alt>+q': on_active_quit})
-hotkey_toggle   = pynput.keyboard.GlobalHotKeys({'<alt>+]': on_active_toggle})
+hotkey_toggle   = pynput.keyboard.GlobalHotKeys({'<ctrl>+q': on_active_toggle})
+hotkey_next     = pynput.keyboard.GlobalHotKeys({'<ctrl>+]': next_loadout}) 
+hotkey_prev     = pynput.keyboard.GlobalHotKeys({'<ctrl>+[': prev_loadout})
 listner         = pynput.keyboard.Listener(on_press=on_press)
 
 # start lisnters
 hotkey_quit.start()
 hotkey_toggle.start()
+hotkey_next.start()
+hotkey_prev.start()
 listner.start()
 
 # start threads
 hotkey_quit.join()
 hotkey_toggle.join()
+hotkey_next.join()
+hotkey_prev.join()
 listner.join()
